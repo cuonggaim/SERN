60a8a8f2a9a4ce093c111e11ef86e60a746b12db
CuongGaim <cuonggaim@gmail.com>
2022-03-16 16:10:35 +0700

    Phan dau tien

---
+++ src/migrations/20220316044239-create-user.js
@@ -0,0 +1,15 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    return queryInterface.bulkInsert('Users', [{
+      firstName: 'John',
+      lastName: 'Doe',
+      email: 'example@example.com',
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }]);
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Users');
+  }
+};
