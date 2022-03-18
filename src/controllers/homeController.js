4c4d8358207ca28d705da65a788ae985a06eb42e
CuongGaim <cuonggaim@gmail.com>
2022-03-18 14:35:35 +0700

    ngay 3

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,4 +1,6 @@
 import db from '../models/index';
+import CRUDService from '../services/CRUDServices'
+
 let getHomePage = async (req, res) => {
     try{
         let data = await db.User.findAll();
@@ -16,7 +18,19 @@ let getAboutPage = (req, res) => {
     return res.render('test/about.ejs');
 }
 
+let getCRUD = (req, res) => {
+    return res.render('crud.ejs'); 
+}
+
+let postCRUD = async (req, res) => {
+    let message = await CRUDService.createNewUser(req.body);
+    console.log(message);
+    return res.send(`Post CRUD from server`);
+}
+
 module.exports = {
     getHomePage: getHomePage,
-    getAboutPage: getAboutPage
+    getAboutPage: getAboutPage,
+    getCRUD: getCRUD,
+    postCRUD: postCRUD,
 }
