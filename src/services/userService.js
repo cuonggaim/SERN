ac8ea0ee644842be59a6155377940ae74c9b97aa
CuongGaim <cuonggaim@gmail.com>
2022-03-31 08:54:13 +0700

    Sua loi daataabase

--- src/services/userService.js
+++ src/services/userService.js
@@ -26,7 +26,7 @@ let handleUserLogin = (email,password) => {
                 //compare password
 
                 let user = await db.User.findOne({
-                    attributes: [`email`, `roleId`, `password`],
+                    attributes: [`email`, `roleId`, `password`, `firstName`, `lastName`],
                     where:{ email:email},
                     raw: true
                 });
@@ -121,8 +121,9 @@ let createNewUser = (data) => {
                     lastName: data.lastName,
                     address: data.address,
                     phonenumber: data.phonenumber,
-                    gender: data.gender === '1' ? true : false,
-                    roleId: data.roleId
+                    gender: data.gender,
+                    roleId: data.roleId,
+                    positionId: data.positionId,
                 })
 
                 resolve({
