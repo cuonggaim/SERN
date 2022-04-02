c9b44cc8020015beaf834808d2895476ae55b43b
CuongGaim <cuonggaim@gmail.com>
2022-04-02 09:22:04 +0700

    Tao api trich xuat top doctor

---
+++ src/services/doctorService.js
@@ -0,0 +1,32 @@
+import db from '../models/index';
+
+let getTopDoctorHome = (limitInput) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            let users = await db.User.findAll({
+                limit: limitInput,
+                where: {roleId: 'R2'},
+                order: [['createdAt', 'DESC']],
+                attributes: {
+                    exclude: [`password`]
+                },
+                include: [
+                    { model: db.Allcode, as: 'positionData', attribute: ['valueEn','valueVi'] },
+                    { model: db.Allcode, as: 'genderData', attribute: ['valueEn','valueVi'] }
+                ],
+                raw: true,
+                nest: true
+            })
+            resolve({
+                errCode: 0,
+                data: users
+            })
+        } catch (error) {
+            reject(error);
+        }
+    })
+}
+
+module.exports = {
+    getTopDoctorHome: getTopDoctorHome
+}
