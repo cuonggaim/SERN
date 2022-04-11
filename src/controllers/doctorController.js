11fbaa69717026c6186938b1912a99be1cfb2326
CuongGaim <cuonggaim@gmail.com>
2022-04-11 20:07:55 +0700

    Api load dong thong tin bac so

--- src/controllers/doctorController.js
+++ src/controllers/doctorController.js
@@ -92,6 +92,18 @@ let getExtraInfoDoctorById = async (req, res) => {
     }
 };
 
+let getProfileDoctorById = async (req, res) => {
+    try {
+        let info = await doctorService.getProfileDoctorById(req.query.doctorId);
+        return res.status(200).json(info);
+    } catch (error) {
+        return res.status(200).json({
+            errCode: -1,
+            errMessage: `Error from server`,
+        });
+    }
+};
+
 module.exports = {
     getTopDoctorHome: getTopDoctorHome,
     getAllDoctors: getAllDoctors,
@@ -100,4 +112,5 @@ module.exports = {
     bulkCreateSchedule: bulkCreateSchedule,
     getScheduleByDate: getScheduleByDate,
     getExtraInfoDoctorById: getExtraInfoDoctorById,
+    getProfileDoctorById: getProfileDoctorById,
 };
