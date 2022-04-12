521333c69b46e31ab1ad516e0cc5719a542b0a7e
CuongGaim <cuonggaim@gmail.com>
2022-04-12 19:57:28 +0700

    Viet api luu thong tin dat lich kham benh

--- src/route/web.js
+++ src/route/web.js
@@ -2,7 +2,7 @@ import express from "express";
 import homeController from "../controllers/homeController";
 import userController from "../controllers/userController";
 import doctorController from "../controllers/doctorController";
-import { route } from "express/lib/application";
+import patientController from "../controllers/patientController";
 
 let router = express.Router();
 
@@ -46,6 +46,11 @@ let initWebRoutes = (app) => {
     router.get(
         "/api/get-profile-doctor-by-id",
         doctorController.getProfileDoctorById
+    );
+
+    router.post(
+        "/api/patient-book-appointment",
+        patientController.postBookAppointment
     );
 
     return app.use("/", router);
