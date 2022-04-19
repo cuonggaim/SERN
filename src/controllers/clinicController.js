85b54ff5b398faee54abbb62919ec7ffe735c64e
CuongGaim <cuonggaim@gmail.com>
2022-04-20 03:29:17 +0700

    Hoan thien chi tiet phong kham

--- src/controllers/clinicController.js
+++ src/controllers/clinicController.js
@@ -12,6 +12,32 @@ let createClinic = async (req, res) => {
     }
 };
 
+let getAllClinic = async (req, res) => {
+    try {
+        let info = await clinicService.getAllClinic();
+        return res.status(200).json(info);
+    } catch (error) {
+        return res.status(200).json({
+            errCode: -1,
+            errMessage: `Error from server`,
+        });
+    }
+};
+
+let getDetailClinicById = async (req, res) => {
+    try {
+        let info = await clinicService.getDetailClinicById(req.query.id);
+        return res.status(200).json(info);
+    } catch (error) {
+        return res.status(200).json({
+            errCode: -1,
+            errMessage: `Error from server`,
+        });
+    }
+};
+
 module.exports = {
     createClinic: createClinic,
+    getAllClinic: getAllClinic,
+    getDetailClinicById: getDetailClinicById,
 };
